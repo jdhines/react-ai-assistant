cd30b596e606c6fe8ef90b9f855801ca2ce168a4
jdhines <jessdhines@gmail.com>
2025-05-29 13:39:34 -0700

    Update a few styles for layout

--- src/routes/new-request.tsx
+++ src/routes/new-request.tsx
@@ -1,21 +1,23 @@
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 import { createFileRoute } from "@tanstack/react-router";
 import { BackButton } from "~/components/BackButton";
 
 export const Route = createFileRoute("/new-request")({
-  head: () => ({
-    meta: [{ title: "New Request" }],
-  }),
-  component: NewRequestPage,
-})
+	head: () => ({
+		meta: [{ title: "New Request" }],
+	}),
+	component: NewRequestPage,
+});
 function NewRequestPage() {
-  return (
-    <div>
-      <div className="flex justify-between mb-8">
-        <BackButton to="/" />
-      </div>
-      <Typography variant="h2" gutterBottom>New Request</Typography>
-      <p>Create a new request with HQ Assistant!</p>
-    </div>
-  );
+	return (
+		<div className="px-4 pb-4">
+			<div className="flex justify-between mb-8">
+				<BackButton to="/" />
+			</div>
+			<Typography variant="h2" gutterBottom>
+				New Request
+			</Typography>
+			<p>Create a new request with HQ Assistant!</p>
+		</div>
+	);
 }
--- src/routes/track-request.tsx
+++ src/routes/track-request.tsx
@@ -3,20 +3,20 @@ import { createFileRoute } from "@tanstack/react-router";
 import { BackButton } from "~/components/BackButton";
 
 export const Route = createFileRoute("/track-request")({
-  head: () => ({
-    meta: [{ title: "Track Request" }],
-  }),
-  component: TrackRequestPage,
-})
+	head: () => ({
+		meta: [{ title: "Track Request" }],
+	}),
+	component: TrackRequestPage,
+});
 
 function TrackRequestPage() {
-  return (
-    <div>
-      <div className="flex justify-between mb-8">
-        <BackButton to="/" />
-      </div>
-      <Typography variant="h2">Track Request</Typography>
-      <p>Track your requests</p>
-    </div>
-  );
+	return (
+		<div className="px-4 pb-4">
+			<div className="flex justify-between mb-8">
+				<BackButton to="/" />
+			</div>
+			<Typography variant="h2">Track Request</Typography>
+			<p>Track your requests</p>
+		</div>
+	);
 }
