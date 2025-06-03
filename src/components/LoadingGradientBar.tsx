06125d943c428767b0a8d8147ad76bb412461cf7
jdhines <jessdhines@gmail.com>
2025-06-03 15:17:10 -0700

    Fix window sizing problems when expanded

--- src/components/LoadingGradientBar.tsx
+++ src/components/LoadingGradientBar.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./LoadingGradientBar.css";
 
 export function LoadingGradientBar() {
-  return (
-    <div className="w-1/3 max-w-full h-2 rounded-full overflow-hidden bg-gray-200">
-      <div className="loading-gradient-bar h-full w-full animate-gradient-x" />
-    </div>
-  );
+	return (
+		<div className="w-1/2 max-w-full h-2 rounded-full overflow-hidden bg-gray-200">
+			<div className="loading-gradient-bar h-full w-full animate-gradient-x" />
+		</div>
+	);
 }
